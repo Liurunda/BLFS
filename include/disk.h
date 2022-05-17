d3e518c60156b7ae0a88749dd0562be4372839a2
flagerlee <flagerlee@163.com>
2022-05-17 17:16:28 +0800

    implement required api

--- include/disk.h
+++ include/disk.h
@@ -46,12 +46,14 @@ public:
     int get_metadata_size(int group_id);
 
     void update_inode(int inode_id);
-    void updata_data(int block_id, void *data);    
-    int acquire_unused_block();//空block编号
-    int acquire_unused_inode();
-    void release_inode(int inode_id);
-    void release_block(int block_id);
 
+    void update_data(int block_id, void *data);
+
+    int acquire_unused_block(); //空block编号
+
+    int acquire_unused_inode();
+
+    void release_block(int block_id);
 
 
     static const ull BOOTBLOCK_OFFSET = 0;
@@ -62,6 +64,7 @@ public:
 
     BlockGroup *block_group;
     int num_block_group;
+    ull block_size;
     ull block_group_size;
 private:
     static Disk *disk_instance;
@@ -69,6 +72,12 @@ private:
     Disk();
 
     ~Disk();
+
+    int write_block(int block_id, void *data);
+
+    void *block_buf;
+    void *buf; // pre-allocated enough space to serialize metadata
+    int disk_fd;
 };
 
 #endif //BLFS_DISK_H
